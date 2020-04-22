11af3a082f4ee3f9065d9f813b15b2c3bb7904db
Sahil Goel <sahil.goel@nagarro.com>
2020-04-22 19:24:30 +0530

    final commit

---
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Todo } from '../../assignment.component';
+
+@Component({
+  selector: 'app-assignment2',
+  templateUrl: './assignment2.component.html',
+  styleUrls: ['./assignment2.component.css']
+})
+export class Assignment2Component {
+  @Input()
+  todoItem: Todo[];
+  @Input()
+  selectedTodo: Todo;
+  onselectTodo(todo: Todo) {
+    this.selectedTodo = todo;
+  }
+}
